52ce9f6b3469393d76e9309edf23cfc173005a13
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-11 09:30:55 -0400

    milestone:added controller actions in flight controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,19 @@
 class FlightsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        flights = Flight.all
+        render json: flights, status: :ok
+    end
+
+    def show
+        flight = Flight.find_by!(id: params[:id])
+        render json: flight, status: :ok
+    end
+
+    private
+
+    def render_not_found_response
+        render json: {error: "flight not found"}, status: :not_found
+    end
 end
