2545ee6d62a3fbd2a55eb59921bc8b9f46af28f6
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-11 14:40:21 -0400

    milestone:adding controller actions in user profile controller

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,3 +1,22 @@
 class UserProfilesController < ApplicationController
-    
+    def create
+        user_profile = UserProfile.create(userprofile_params)
+        if user_profile.valid?
+            render json: user_profile, status: :created
+        else
+            render json: {errors: user_profile.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        user_profile = UserProfile.find_by!(id: params[:id])
+        user_profile.update!(update_params)
+        render json: user_profile, status: :created
+    end
+
+    private
+
+    def userprofile_params
+        params.permit(:name, :password)
+    end
 end
